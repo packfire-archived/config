4c834a08c7350d45e9ce0b307e5a531af46f827f
Sam-Mauris Yong <mauris@hotmail.sg>
2013-08-25 16:13:29 +0800

    ini write test implementation

--- test/Packfire/Config/Driver/IniTest.php
+++ test/Packfire/Config/Driver/IniTest.php
@@ -22,15 +22,36 @@ class IniTest extends ConfigTestSetter
 data=5
 route=false
 EOT;
-        
         $root = vfsStream::setup('root');
         $file = vfsStream::newFile('config.ini');
         $root->addChild($file);
         $file->withContent($data);
-        
         $reader = new Ini(vfsStream::url('root/config.ini'));
         $reader->read();
 
         $this->assertEquals(array('test' => array('data' => 5, 'route' => false)), $reader->get());
+    }
+
+    public function testWrite()
+    {
+        $root = vfsStream::setup('root');
+        $file = vfsStream::newFile('config.ini');
+        $root->addChild($file);
+
+        $reader = new Ini(vfsStream::url('root/config.ini'));
+        $reader->set('test', 'epic', 5);
+        $reader->set('alpha', 'bravo', 5);
+
+        $reader->write();
+
+        $expected = array(
+            'test' => array(
+                'epic' => 5
+            ),
+            'alpha' => array(
+                'bravo' => 5
+            )
+        );
+        $this->assertEquals($expected, parse_ini_file(vfsStream::url('root/config.ini'), true));
     }
 }
