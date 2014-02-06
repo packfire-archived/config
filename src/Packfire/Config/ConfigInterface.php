8fe400756be197874e002f6bad112f0ebe2cce77
Sam-Mauris Yong <mauris@hotmail.sg>
2014-02-06 22:53:14 +0800

    renamed IConfig to ConfigInterface

--- src/Packfire/Config/ConfigInterface.php
+++ src/Packfire/Config/ConfigInterface.php
@@ -3,7 +3,7 @@
 /**
  * Packfire Framework for PHP
  * By Sam-Mauris Yong
- * 
+ *
  * Released open source under New BSD 3-Clause License.
  * Copyright (c) Sam-Mauris Yong <sam@mauris.sg>
  * All rights reserved.
@@ -20,22 +20,21 @@ namespace Packfire\Config;
  * @package Packfire\Config
  * @since 1.0.0
  */
-interface IConfig {
-    
+interface ConfigInterface
+{
     /**
      * Get the value from the configuration file.
-     * 
+     *
      * You can get values nested inside arrays by entering multiple keys as
      * arguments to the method.
-     * 
+     *
      * Example:
      * <code>$value = $config->get('app', 'name'); // $data = array('app' => array('name' => 'Packfire')); </code>
      * <code>$value = $config->get('database', 'default', 'host'); // $data = array('database' => array('default' => array('host' => 'localhost'))); </code>
-     * 
-     * @param string $key,... The key of the data to load. 
+     *
+     * @param string $key,... The key of the data to load.
      * @return mixed Returns the data read or NULL if the key is not found.
      * @since 1.0.0
      */
     public function get();
-    
 }
