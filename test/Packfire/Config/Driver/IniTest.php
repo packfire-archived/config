fc93c9ab5ad2f107123c3c751699b76f4996a4d8
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-02 01:37:36 +0800

    updated ini test

--- test/Packfire/Config/Driver/IniTest.php
+++ test/Packfire/Config/Driver/IniTest.php
@@ -5,16 +5,23 @@ namespace Packfire\Config\Driver;
 use org\bovigo\vfs\vfsStream;
 use PHPUnit_Framework_TestCase;
 
-class IniTest extends ConfigTestSetter
+class IniTest extends PHPUnit_Framework_TestCase
 {
-    protected function setUp()
-    {
-        $this->prepare('Packfire\\Config\\Driver\\Ini');
-    }
-
     public function testFile()
     {
-        $this->assertEquals($this->file, $this->object->file());
+        $data = <<<EOT
+[test]
+data=5
+route=false
+EOT;
+        $root = vfsStream::setup('root');
+        $file = vfsStream::newFile('config.ini');
+        $root->addChild($file);
+        $file->withContent($data);
+        $file = vfsStream::url('root/config.ini');
+        $config = new Ini($file);
+        $config->read();
+        $this->assertEquals($file, $config->file());
     }
 
     public function testRead()
@@ -28,10 +35,10 @@ EOT;
         $file = vfsStream::newFile('config.ini');
         $root->addChild($file);
         $file->withContent($data);
-        $reader = new Ini(vfsStream::url('root/config.ini'));
-        $reader->read();
+        $config = new Ini(vfsStream::url('root/config.ini'));
+        $config->read();
 
-        $this->assertEquals(array('test' => array('data' => 5, 'route' => false)), $reader->get());
+        $this->assertEquals(array('test' => array('data' => 5, 'route' => false)), $config->get());
     }
 
     public function testWrite()
@@ -40,11 +47,11 @@ EOT;
         $file = vfsStream::newFile('config.ini');
         $root->addChild($file);
 
-        $reader = new Ini(vfsStream::url('root/config.ini'));
-        $reader->set('test', 'epic', 5);
-        $reader->set('alpha', 'bravo', 5);
+        $config = new Ini(vfsStream::url('root/config.ini'));
+        $config->set('test', 'epic', 5);
+        $config->set('alpha', 'bravo', 5);
 
-        $reader->write();
+        $config->write();
 
         $expected = array(
             'test' => array(
